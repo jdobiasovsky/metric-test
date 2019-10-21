f91cfcb9bc788deae2021ef79bbf30cbbaa87514
jdobiasovsky <jan.dobiasovsky@techlib.cz>
2019-10-21 17:06:28 +0200

    Added convenience script running entire pre-processing phase

---
+++ src/rlink.R
@@ -0,0 +1,11 @@
+# Convenience script that loads dataset and performs record linkage for all 5 metrics one after another
+source("src/preparation/load_dataset.R")
+source("src/preparation/split_dataset.R")
+rm(dataset_clean_human, dataset_raw, reclink_data, reclink_CROSSREF, envir = .GlobalEnv)
+source("src/processing/crunch_data.R")
+
+for (mtrc in c("lv", "jaro", "jw", "jaccard", "qgram")){
+  for (year in 1950:2019){
+    crunch(year = year, metric = mtrc, stringLength = TRUE)
+  }
+}
