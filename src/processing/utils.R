2f179358ca37bbc2a87965fb24044eeb24de3c8b
jdobiasovsky <jan.dobiasovsky@techlib.cz>
2019-11-03 18:37:44 +0100

    Function for easy file path building

--- src/processing/utils.R
+++ src/processing/utils.R
@@ -12,6 +12,10 @@ open_multiple <- function(list){
   return(do.call(rbind,lapply(list,open_data)))
 }
 
+build_path <- function(base, metric,year, extension){
+  return(paste(base, metric, "_", year, extension, sep = ""))
+}
+
 filter_tn <- function(path, column){
   # filter out true negatives for given column, keep info
   data <- open_data(path)
@@ -19,4 +23,3 @@ filter_tn <- function(path, column){
   data <- as_tibble(cbind(data[1:7],data[column])) %>% filter(!(UQ(as.symbol(column)) >= 0.11 & DOI1!=DOI2))
   write_csv(x = data, path = paste("./data/precision_recall/", basename(path), sep = ""))
 }
-
