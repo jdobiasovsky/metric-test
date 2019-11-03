bec8ad6e319f79be58b52bc2a1cf4a254a2e13b3
jdobiasovsky <jan.dobiasovsky@techlib.cz>
2019-11-03 14:56:59 +0100

    Fix wrong true negative filtering, add opening for multiple files at once into single data_frame

--- src/processing/utils.R
+++ src/processing/utils.R
@@ -8,10 +8,15 @@ open_data <- function(input){
   return(data)
 }
 
+open_multiple <- function(list){
+  return(do.call(rbind,lapply(list,open_data)))
+}
+
 filter_tn <- function(path, column){
   # filter out true negatives for given column, keep info
   data <- open_data(path)
   # Filter out true negative matches (DOI's don't match and are below lowest considered treshold)
-  data <- as.tibble(cbind(data[1:7],data[column])) %>% filter(DOI1!=DOI2 & UQ(as.symbol(column)) <= 0.11)
+  data <- as_tibble(cbind(data[1:7],data[column])) %>% filter(!(UQ(as.symbol(column)) >= 0.11 & DOI1!=DOI2))
   write_csv(x = data, path = paste("./data/precision_recall/", basename(path), sep = ""))
 }
+
