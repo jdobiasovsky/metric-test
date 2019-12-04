9a97c605ff39e37157d32d5468cefef75e6941ac
jdobiasovsky <jan.dobiasovsky@techlib.cz>
2019-12-04 14:48:21 +0100

    moved filter_tn to standardization

--- src/processing/utils.R
+++ src/processing/utils.R
@@ -12,14 +12,7 @@ open_multiple <- function(list){
   return(do.call(rbind,lapply(list,open_data)))
 }
 
-build_path <- function(base, metric,year, extension){
+build_path <- function(base, metric, year, extension){
   return(paste(base, metric, "_", year, extension, sep = ""))
 }
 
-filter_tn <- function(path, column){
-  # filter out true negatives for given column, keep info
-  data <- open_data(path)
-  # Filter out true negative matches (DOI's don't match and are below lowest considered treshold)
-  data <- as_tibble(cbind(data[1:7],data[column])) %>% filter(!(UQ(as.symbol(column)) >= 0.11 & DOI1!=DOI2))
-  write_csv(x = data, path = paste("./data/precision_recall/", basename(path), sep = ""))
-}
