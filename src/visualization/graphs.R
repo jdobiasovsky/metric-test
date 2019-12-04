e31b548be5150dcbc34cd0ad3c89dc4b459fa58f
jdobiasovsky <jan.dobiasovsky@techlib.cz>
2019-12-04 14:48:59 +0100

    added cosine4 layer

--- src/visualization/graphs.R
+++ src/visualization/graphs.R
@@ -6,6 +6,7 @@ show_perfomance_years_overview <- function(metric){
   data2016 <- generate_results_merge(metric, 2016, "TITLE")
   data2017 <- generate_results_merge(metric, 2017, "TITLE")
   data2018 <- generate_results_merge(metric, 2018, "TITLE")
+  
   ggplot() + 
     geom_smooth(aes(x=data2000_2010$Recall, y=data2000_2010$Precision, color="2000-2010"), method="auto") +
     geom_smooth(aes(x=data2010_2015$Recall, y=data2010_2015$Precision, color="2010-2015"), method="auto") +
@@ -22,13 +23,18 @@ show_perfomance_metrics_overview <- function(year){
   data_jw <- generate_results_merge("jw", year, "TITLE")
   data_jaro <- generate_results_merge("jaro", year, "TITLE")
   data_cosine3 <- generate_results_merge("cosine3", year, "TITLE")
+  data_cosine4 <- generate_results_merge("cosine4", year, "TITLE")
   data_jaccard3 <- generate_results_merge("jaccard3", year, "TITLE")
+  data_jaccard4 <- generate_results_merge("jaccard4", year, "TITLE")
+  
   ggplot() + 
     geom_smooth(aes(x=data_lv$Recall, y=data_lv$Precision, color="lv"), method="auto") +
     geom_smooth(aes(x=data_jaro$Recall, y=data_jaro$Precision, color="jaro"), method="auto") +
     geom_smooth(aes(x=data_jw$Recall, y=data_jw$Precision, color="jw"), method="auto") +
     geom_smooth(aes(x=data_cosine3$Recall, y=data_cosine3$Precision, color="cosine3"), method="auto") +
+    geom_smooth(aes(x=data_cosine4$Recall, y=data_cosine4$Precision, color="cosine4"), method="auto") +
     geom_smooth(aes(x=data_jaccard3$Recall, y=data_jaccard3$Precision, color="jaccard3"), method="auto") +
+    geom_smooth(aes(x=data_jaccard4$Recall, y=data_jaccard4$Precision, color="jaccard4"), method="auto") +
     labs(title="Precision-Recall curve plot",
          x="Recall", y="Precision")
 }
@@ -42,6 +48,7 @@ show_perfomance_metrics_line <- function(year, graph_title="Precision-Recall cur
   data_jw <- generate_results_merge("jw", year, "TITLE")
   data_jaro <- generate_results_merge("jaro", year, "TITLE")
   data_cosine3 <- generate_results_merge("cosine3", year, "TITLE")
+  data_cosine4 <- generate_results_merge("cosine4", year, "TITLE")
   data_jaccard3 <- generate_results_merge("jaccard3", year, "TITLE")
   data_jaccard4 <- generate_results_merge("jaccard4", year, "TITLE")
   
@@ -50,6 +57,7 @@ show_perfomance_metrics_line <- function(year, graph_title="Precision-Recall cur
     geom_line(aes(x=data_jaro$Recall, y=data_jaro$Precision, color="jaro")) +
     geom_line(aes(x=data_jw$Recall, y=data_jw$Precision, color="jw")) +
     geom_line(aes(x=data_cosine3$Recall, y=data_cosine3$Precision, color="cosine3")) +
+    geom_line(aes(x=data_cosine4$Recall, y=data_cosine4$Precision, color="cosine4")) +
     geom_line(aes(x=data_jaccard3$Recall, y=data_jaccard3$Precision, color="jaccard3")) +
     geom_line(aes(x=data_jaccard4$Recall, y=data_jaccard4$Precision, color="jaccard4")) +
     labs(title=graph_title,
@@ -63,6 +71,7 @@ show_perfomance_metrics_point <- function(year, graph_title="Precision-Recall cu
   data_jw <- generate_results_merge("jw", year, "TITLE")
   data_jaro <- generate_results_merge("jaro", year, "TITLE")
   data_cosine3 <- generate_results_merge("cosine3", year, "TITLE")
+  data_cosine4 <- generate_results_merge("cosine4", year, "TITLE")
   data_jaccard3 <- generate_results_merge("jaccard3", year, "TITLE")
   data_jaccard4 <- generate_results_merge("jaccard4", year, "TITLE")
   
@@ -71,6 +80,7 @@ show_perfomance_metrics_point <- function(year, graph_title="Precision-Recall cu
     geom_point(aes(x=data_jaro$Recall, y=data_jaro$Precision, color="jaro")) +
     geom_point(aes(x=data_jw$Recall, y=data_jw$Precision, color="jw")) +
     geom_point(aes(x=data_cosine3$Recall, y=data_cosine3$Precision, color="cosine3")) +
+    geom_point(aes(x=data_cosine4$Recall, y=data_cosine4$Precision, color="cosine4")) +
     geom_point(aes(x=data_jaccard3$Recall, y=data_jaccard3$Precision, color="jaccard3")) +
     geom_point(aes(x=data_jaccard4$Recall, y=data_jaccard4$Precision, color="jaccard4")) +
     labs(title=graph_title,
@@ -83,6 +93,7 @@ show_fmeasure_treshold <- function(year, graph_title="F-Measure plot"){
   data_jw <- generate_results_merge("jw", year, "TITLE")
   data_jaro <- generate_results_merge("jaro", year, "TITLE")
   data_cosine3 <- generate_results_merge("cosine3", year, "TITLE")
+  data_cosine4 <- generate_results_merge("cosine4", year, "TITLE")
   data_jaccard3 <- generate_results_merge("jaccard3", year, "TITLE")
   data_jaccard4 <- generate_results_merge("jaccard4", year, "TITLE")
   
@@ -91,10 +102,11 @@ show_fmeasure_treshold <- function(year, graph_title="F-Measure plot"){
     geom_line(aes(x=data_jaro$Treshold, y=data_jaro$Fmeasure, color="jaro")) +
     geom_line(aes(x=data_jw$Treshold, y=data_jw$Fmeasure, color="jw")) +
     geom_line(aes(x=data_cosine3$Treshold, y=data_cosine3$Fmeasure, color="cosine3")) +
+    geom_line(aes(x=data_cosine3$Treshold, y=data_cosine4$Fmeasure, color="cosine4")) +
     geom_line(aes(x=data_jaccard3$Treshold, y=data_jaccard3$Fmeasure, color="jaccard3")) +
     geom_line(aes(x=data_jaccard4$Treshold, y=data_jaccard4$Fmeasure, color="jaccard4")) +
     labs(title=graph_title,
-         x="Treshold", y="F-Measure")
+         x="Treshold", y="F-Measure") 
 }
 
 
@@ -104,13 +116,9 @@ draw_results_graph <- function(metric, year, graph_title="Precision-Recall curve
 
   ggplot(data) + 
     geom_line(aes(x=Recall, y=Precision)) +
-    geom_label_repel(aes(x=Recall, y=Precision,label=Treshold), data=data[seq(1, nrow(data), 100), ],
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             box.padding   = 0.35, 
+    geom_label_repel(aes(x=Recall, y=Precision,label=Treshold), data=data[seq(1, nrow(data), 100), ],                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            box.padding   = 0.35, 
                      point.padding = 0.5,
                      segment.color = 'grey50')+
     labs(title=graph_title,
          x="Recall", y="Precision")
 }
-
-
-
