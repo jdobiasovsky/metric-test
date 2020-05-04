6dce1a2354708d312770162d7e5f812c577ce022
jdobiasovsky <dobiasovsky.honza@gmail.com>
2020-05-04 23:50:27 +0200

    Moved results.R to more sensible location

--- src/visualization/setenv.R
+++ src/visualization/setenv.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(readr)
 
 source("./src/processing/utils.R")
-source("./src/visualization/results.R")
+source("./src/processing/results.R")
 source("./src/visualization/graphs_offload.R")
 
 
